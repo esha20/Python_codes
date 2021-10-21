88dbc01bdfbed1fa1c8140afa03a7ccc813d8077
Esha Mitra <72601896+esha20@users.noreply.github.com>
2021-10-21 18:34:49 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,42 @@
+# Arithmetic operators
+print("The result of 3+4 is", 3+4)
+print("The result of 3-4 is", 3-4)
+print("The result of 3*4 is", 3*4)
+print("The result of 3/4 is", 3/4)
+# Assignment operators
+a = 32
+a += 3
+print(a)
+b = 23
+b -= 3
+print(b)
+c = 80
+c *= 2
+print(c)
+d = 12
+d /= 2
+print(d)
+'''if we divide any divisible number by any other number just like here, the output is a float number
+comparison operators'''
+e=(3 > 6)
+print(e)
+f = (3 < 6)
+print(f)
+g = (3 >= 6)
+print(g)
+h = (3 <= 6)
+print(h)
+i = (3 == 6)
+print(i)
+j = (3 != 6)
+print(j)
+# logical operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is ", (bool1 and bool2))
+print("The value of bool1 or bool2 is ", (bool1 or bool2))
+print("The value of not bool1 , not bool2 are respectively ", (not bool2), (not bool1))
+print(10//3) #division gives integer
+print(10/3) #division gives float
+print(10*3) #simple multiplication
+print(10**3) #becomes exponential equation
